36160b8a3172215273767f036c5a6c0ba5736954
mohammedazeem1501 <mohammed.azeem@tothenew.com>
2020-03-03 20:10:03 +0530

    ADO.NET DONE

---
+++ Assignment/ADO.NET/Q_07/WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Q_07.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            <br />
+            <br />
+            <asp:GridView ID="GridView2" runat="server"></asp:GridView>
+
+            <br />
+            <br />
+            <br />
+
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Assignment/ADO.NET/Q_07/WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+           
+namespace Q_07
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connect = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            if(Cache["value"] == null)
+            {
+                using (SqlConnection con = new SqlConnection(connect))
+                {
+                    SqlDataAdapter ada = new SqlDataAdapter("select * from orders",con);
+                    DataSet ds = new DataSet();
+                    ada.Fill(ds);
+
+                    GridView1.DataSource = ds;
+                    GridView1.DataBind();
+
+                    Cache["value"] = ds;
+                    Label1.Text = "data is loaded from the DB";
+                }
+            }
+            else
+            {
+                GridView2.DataSource = (DataSet)Cache["value"];
+                GridView2.DataBind();
+                Label1.Text = "data is loaded from the cache only.....!!";
+            }
+        }
+    }
+}

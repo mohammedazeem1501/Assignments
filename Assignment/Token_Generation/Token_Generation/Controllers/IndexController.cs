04cc346fc9ab11e933e0e7fee326d593c9aa2d8b
mohammedazeem1501 <mohammed.azeem@tothenew.com>
2020-03-17 12:46:27 +0530

    JWT Token Assignment Submitted

---
+++ Assignment/Token_Generation/Token_Generation/Controllers/IndexController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Token_Generation.Data;
+using Token_Generation.Models;
+
+namespace Token_Generation.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        private readonly IToken_Generate _Generate;
+        public IndexController(IToken_Generate token_Generate)
+        {
+            _Generate = token_Generate;
+        }
+
+        [HttpPost]
+        public IActionResult Token(LoginDetails details)
+        {
+            try
+            {
+                return Ok(_Generate.Generate_Token(details));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                string result = "hey, this is the Get()";
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetCommon()
+        {
+
+            try
+            {
+                return Ok("Hey , this method can be invoked without any authorization...........!!!");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")] // role se bulaya hai .
+        public IActionResult GetADmin()
+        {
+            try
+            {
+
+                return Ok("Hey, Welcome... admin....!!! ");
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "user")] // role se bulaya hai .
+        public IActionResult GetUser()
+        {
+
+            try
+            {
+                return Ok("Hey, Welcome ....You have Logged In as User ");
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Assignment/Token_Generation/Token_Generation/Data/Token_Generate.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Token_Generation.Models;
+
+namespace Token_Generation.Data
+{
+    public class Token_Generate : IToken_Generate
+    {
+      
+        public IConfiguration _config;
+
+        public Token_Generate(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private LoginDetails GetUser(string username, string password,string role)
+        {
+            var _context = new TokenDBContext();
+            return _context.LoginDetails.FirstOrDefault(u => u.UserName == username && u.Password == password && u.Role == role);
+        }
+        public string Generate_Token(LoginDetails login)
+        {
+            
+            var user = GetUser(login.UserName, login.Password,login.Role);  // username = Admin / Azeem && password = pass
+            if (user != null)
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Role,login.Role) // role = admin/user
+                   
+                };
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims: claims,
+                expires: DateTime.Now.AddSeconds(60),
+                signingCredentials: credentials);
+
+                return (new JwtSecurityTokenHandler().WriteToken(token));  
+
+
+            }
+            else
+            {
+                return("Something Went Wrong.................@@!!!!!");
+            }
+
+
+        }
+    }
+}
